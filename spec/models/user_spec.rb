ab0e613b2675eaeaf5e8d411a0476b5f26bd19ce
Fernando Villalobos <fernando.villalobos@logicalbricks.com>
2015-04-25 12:08:37 -0500

    Creating User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { should validate_presence_of :email }
+  it { should respond_to :password }
+  it { should respond_to :password_confirmation }
+  it { should validate_presence_of(:password).on :create }
+end
